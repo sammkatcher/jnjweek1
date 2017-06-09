3f769bf1c570722be61e0d63a22b20b6b882a997
rorydod <rorydod>
2017-06-09 11:51:46 -0400

    date fix

--- endomech_practice.R
+++ endomech_practice.R
@@ -9,6 +9,7 @@ str(endomech)
 #### readline()
 #### https://stackoverflow.com/questions/12550677/in-r-is-there-some-function-like-raw-input-in-python
 column <- "Circular.Stapler.Circular.Stapler.Stryker.Sustainability.Circular.Stapler.excl.PPH"
+col <- endomech$Circular.Stapler.Circular.Stapler.Stryker.Sustainability.Circular.Stapler.excl.PPH
 selected_df <- data.frame(endomech$Date, endomech[column])
 
 # getting a mean from the 'non NA' data in the column
@@ -19,8 +20,14 @@ mean(non_na_selected[1:nrow(non_na_selected),2])
 # changing all the NAs to the calculated mean
 selected_df[is.na(selected_df)] <- mean(non_na_selected[1:nrow(non_na_selected),2])
 
+# change date column from factor variable to an actual graphable date
+selected_df$endomech.Date <- as.Date(selected_df$endomech.Date
+                                          , format = "%m/%d/%Y")
+
 plot(selected_df)
 
+#m1 <- lm(selected_df$endomech.Date ~ selected_df[,2])
+#summary(m1) 
 ## Notes from talking:
 ## Column variable, run the script for any column
 ## figure out rows with NAs
