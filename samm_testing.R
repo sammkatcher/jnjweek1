9407a25567a0642c9c3d82745a542d45d9a25108
Samm <katchs2@rpi.edu>
2017-06-16 10:51:57 -0400

    adding bootstrap files

--- samm_testing.R
+++ samm_testing.R
@@ -1,31 +1,58 @@
+# #load in data
+# endomech <- read.csv("endomech.csv", header = TRUE, strip.white = TRUE)
+# head(endomech)
+# 
+# ## How to access names
+# head(names(endomech))
+# str(endomech)
+# 
+# # making a variable column and then a dataframe with that column and Date
+# # Working example: ## column <- "Circular.Stapler.Circular.Stapler.Stryker.Sustainability.Circular.Stapler.excl.PPH"
+# # factor example Circular.Stapler.Circular.Stapler.Ethicon.Circular.Stapler.excl.PPH
+# column <- readline('Enter column name: ')
+# 
+# selected_df <- data.frame(endomech$Date, endomech[column])
+# ##as.numeric(as.character(selected_df[,2]))
+# 
+# as.num = function(x, na.strings = "NA") {
+#   stopifnot(is.character(x))
+#   na = x %in% na.strings
+#   x[na] = 0
+#   x = as.numeric(x)
+#   x[na] = NA_real_
+#   x
+# }
+# as.num(c("1", "2", "X"), na.strings="X")
+# #[1]  1  2 NA
+# 
+# as.num(c(as.character(selected_df[,2])))
+# 
+# test1 <- as.numeric(test)
+
+
+install.packages('Shiny')
+library(shiny)
+
 #load in data
 endomech <- read.csv("endomech.csv", header = TRUE, strip.white = TRUE)
 head(endomech)
 
-## How to access names
-head(names(endomech))
-str(endomech)
-
-# making a variable column and then a dataframe with that column and Date
-# Working example: ## column <- "Circular.Stapler.Circular.Stapler.Stryker.Sustainability.Circular.Stapler.excl.PPH"
-# factor example Circular.Stapler.Circular.Stapler.Ethicon.Circular.Stapler.excl.PPH
-column <- readline('Enter column name: ')
-
-selected_df <- data.frame(endomech$Date, endomech[column])
-##as.numeric(as.character(selected_df[,2]))
-
-as.num = function(x, na.strings = "NA") {
-  stopifnot(is.character(x))
-  na = x %in% na.strings
-  x[na] = 0
-  x = as.numeric(x)
-  x[na] = NA_real_
-  x
+col_choices <- names(endomech)
+col_choices <- col_choices[2:119]
+ui <- fluidPage(theme = "bootstrap.css",
+                "Endomech Forecasting",
+                selectInput("col_choice", "Column Choice", col_choices),
+                plotOutput("forecast_plot")
+)
+server <- function(input, output){
+  output$forecast_plot <- renderPlot({
+    plot(endomech$input,
+         as.Date(endomech$Date, format = "%m/%d/%Y"))
+  })
 }
-as.num(c("1", "2", "X"), na.strings="X")
-#[1]  1  2 NA
+shinyApp(ui = ui, server = server)
 
-as.num(c(as.character(selected_df[,2])))
 
-test1 <- as.numeric(test)
 
+### When you click the button, get choice of scatter plot and line plot
+### final plot with the red dots for a year out of predicition
