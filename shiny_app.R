2e6121fe319bcd158c994ad31918257d64a47c70
rorydod <rorydod>
2017-06-16 10:40:28 -0400

    shiny

--- shiny_app.R
+++ shiny_app.R
@@ -3,13 +3,14 @@ library(shiny)
 
 col_choices <- names(endomech)
 col_choices <- col_choices[2:119]
-ui <- fluidPage("Endomech Forecasting",
+ui <- fluidPage(
+  h2("Endomech Forecasting"),
                 selectInput("col_choice", "Column Choice", col_choices),
                 plotOutput("forecast_plot")
                 )
 server <- function(input, output){
   output$forecast_plot <- renderPlot({
-    plot(endomech$input,
+    plot(input$col_choice,
          as.Date(endomech$Date, format = "%m/%d/%Y"))
   })
 }
